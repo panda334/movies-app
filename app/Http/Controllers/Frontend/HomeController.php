300932eb2a737be36496f711d9ff18402ec2810d
Yousef Alrefai <yousef.yousee.ref@gmail.com>
2026-06-13 16:29:05 +0300

    Update HomeController.php

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -1,28 +1,36 @@
 <?php
-
 namespace App\Http\Controllers\Frontend;
 
 use App\Models\Movie;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use OpenAdmin\Admin\Grid\Displayers\Input;
 
 class HomeController extends Controller
 {
     public function homePage()
     {
         $movies = Movie::paginate(10);
-        return view('user_interface.index' , compact('movies'));
+        return view('user_interface.index', compact('movies'));
     }
 
     public function search(Request $request)
     {
-        $q = $request->input('q');
-        $movies = Movie::where('name','LIKE','%'.$q.'%')
-                       ->paginate();
-        if(count($movies) > 0)
-            return view('user_interface.index' , compact('movies'))->withDetails($movies)->withQuery ( $q );
-           else
-        return view ('user_interface.index')->withMessage('No Details found. Try to search again !');
+        $request->validate([
+            'q' => ['required', 'string', 'max:255'],
+        ]);
+
+        $q = trim($request->input('q'));
+
+        $movies = Movie::where('name', 'LIKE', '%' . $q . '%')
+                       ->paginate(10); //
+        if ($movies->count() > 0) {
+            return view('user_interface.index', compact('movies', 'q'));
+        }
+
+        return view('user_interface.index', [
+            'movies'  => $movies,
+            'q'       => $q,
+            'message' => 'No results found. Try a different search term.',
+        ]);
     }
 }
